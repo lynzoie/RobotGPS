0fca76bfb84f6325a8386537c599b6824b8c28e2
Echo Lab <echolab@pop-os.localdomain>
2022-05-26 12:21:01 -0400

    incorporated Motion class

--- GPS.py
+++ GPS.py
@@ -5,6 +5,7 @@ import os
 #from DAQ.Observe import Observe
 #from pycreate2 import Create2
 from ViveServer.Client import Client
+from Motion import Motion
 
 IP_ADDRESS = '192.168.8.166' #IP address of elisive-calf on Alfred CAPCOM
 
@@ -27,37 +28,29 @@ class GPS(object):
         theta = np.radians(new_theta)
         return theta
 
+    def find_centerpoint(self, Ax, Ay, Bx, By):
+        Cx = ((Bx - Ax) / 2) + Ax
+        Cy = ((By - Ay) / 2) + Ay
+        return Cx, Cy
+
     def tracker_to_xytheta(self, x1, z1, x2, z2):
-        #### Calculate x-y center coordinates of robot ####
         # x-center of robot is in the middle of the wheel axis (on x-axis)
         # y-center of robot is at 50mm = 0.05m above wheel axis
         # Theta is angle from x-axis that the robot is facing
-        # Create points A and B with (Nx, Ny) format
         Ax = x1
         Ay = z1
 
         Bx = x2
         By = z2
 
-        Cx = ((Bx - Ax) / 2) + Ax
-        Cy = ((By - Ay) / 2) + Ay
-        self.Cx = Cx
-        self.Cy = Cy
-
-        phi = np.degrees(np.arctan2(By-Ay,Bx-Ax))
-        temp_theta = phi + 90
-        new_theta = np.where(temp_theta>180, temp_theta-360, temp_theta)
-        self.theta = np.radians(new_theta)
-
-        self.theta = calc_theta(Ax, Ay, Bx, By)
+        self.Cx, self.Cy = self.find_centerpoint(Ax, Ay, Bx, By)
+        self.theta = self.calc_theta(Ax, Ay, Bx, By)
 
         r = 0.05        # assuming the center of the robot is exactly 50mm from the wheel axis
         # offset to circle at center Cx,Cy
-        self.x = Cx - (r*np.cos(self.theta))        
-        self.y = Cy - (r*np.sin(self.theta))
-
-
-
+        self.x = self.Cx - (r*np.cos(self.theta))        
+        self.y = self.Cy - (r*np.sin(self.theta))
+    
     def travel(self, robot, new_x, new_y):
 
         # Calculate distance and angle robot should turn to get to target
@@ -66,9 +59,13 @@ class GPS(object):
 
         dist = np.linalg.norm(p1 - p2)
         
+        # Rotate to face destination
         phi = np.degrees(np.arctan2(p2[1]-p1[1],p2[0]-p1[0]))
         turn_angle = phi
 
-        # insert rotation command
-        #robot.drive_direct()
-        # insert drive command
+        move_robot = Motion.RobotTranslator(robot)
+        move_robot.turn(a=turn_angle,bot=robot)     # pass angle as degrees
+
+        # Start moving to destination
+        # while self.x != new_x and self.y != new_y
+        move_robot.move(d=dist/2,bot=robot)
