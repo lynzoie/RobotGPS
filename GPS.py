27132ce67d78c95f58c53fcb36dfc32999dd7b04
Echo Lab <echolab@pop-os.localdomain>
2022-05-27 10:59:29 -0400

    removed move_to_dest

--- GPS.py
+++ GPS.py
@@ -4,13 +4,14 @@ import os
 from ViveServer.Client import Client
 from Motion import Motion
 
-IP_ADDRESS = '192.168.8.166' #IP address of elisive-calf on Alfred CAPCOM
+a_arr = []
+b_arr = []
 
 class GPS(object):
     def __init__(self):
         self.x = 0          # x center of robot
         self.y = 0          # y center of robot
-        self.theta = 0      # angle of robot
+        self.theta = 0      # angle of robot in degrees
         
         self.v = 0          # linear velocity, optional attribute
         self.w = 0          # angular velocity, opitional attribute
@@ -18,17 +19,19 @@ class GPS(object):
         self.Cx = 0         # x center between the two wheels, optional attribute
         self.Cy = 0         # y center between the two wheels, optional attribute
 
+
     def calc_theta(self, Ax, Ay, Bx, By):
         phi = np.degrees(np.arctan2(By-Ay,Bx-Ax))
         temp_theta = phi + 90
-        new_theta = np.where(temp_theta>180, temp_theta-360, temp_theta)
-        theta = np.radians(new_theta)
+        theta = np.where(temp_theta>180, temp_theta-360, temp_theta)
         return theta
+
 
     def find_centerpoint(self, Ax, Ay, Bx, By):
         Cx = ((Bx - Ax) / 2) + Ax
         Cy = ((By - Ay) / 2) + Ay
         return Cx, Cy
+
 
     def tracker_to_xytheta(self, x1, z1, x2, z2):
         # x-center of robot is in the middle of the wheel axis (on x-axis)
@@ -47,23 +50,50 @@ class GPS(object):
         # offset to circle at center Cx,Cy
         self.x = self.Cx - (r*np.cos(self.theta))        
         self.y = self.Cy - (r*np.sin(self.theta))
+
+
+    def get_robot_pose(self,c):
+        gps = c.get_trackers_coordinates()
+        a = list(gps['tracker_1'])
+        b = list(gps['tracker_2'])
+        a_arr.append(a[0:3])
+        b_arr.append(b[0:3])
+        
+        self.tracker_to_xytheta(a[0],a[2],b[0],b[2])   # grab current pose of robot
     
-    def travel(self, robot, new_x:'x-coordinate in m', new_y:'y-coordinate in m'):
+
+    def output_gps_coords(self):
+        out_arr = np.vstack((a_arr,b_arr))
+        with open('test_navrobot.txt','w') as f:
+            for item in out_arr:
+                f.write("%s\n" % item)
+
+
+    def rot_to_dest(self,cur_x,cur_y,new_x,new_y, robot, move_robot):
+        # Rotate to face destination
+        phi = np.degrees(np.arctan2(new_y-cur_y , new_x-cur_x))
+        
+        alpha = phi - self.theta               # TODO: fix alpha so we face destination. Consider logic of alpha<0 turns CCW and vice versa
+        # move_robot.turn(a=alpha,bot=robot)     # pass angle as degrees
+
+
+    def travel(self, robot, c, new_x:'x-coordinate in m', new_y:'y-coordinate in m'):
+        # Get inital coordinates in mm
+        self.get_robot_pose(c)
+        conv_x = self.x * 1000
+        conv_y = self.y * 1000
+        print('Conv self.x and self.y: ', conv_x,conv_y)
+        
         # Convert desired xy from m to mm
         new_x = new_x * 1000
         new_y = new_y * 1000
+        print('Desired x and y: ', new_x,new_y)
 
-        # Calculate distance and angle robot should turn to get to target
-        p1 = np.array((self.x,self.y))
-        p2 = np.array((new_x,new_y))
-        dist = np.linalg.norm(p1 - p2)
-        
-        # Rotate to face destination
-        phi = np.degrees(np.arctan2(p2[1]-p1[1],p2[0]-p1[0]))
-        turn_angle = phi
         move_robot = Motion.RobotTranslator(robot)
-        move_robot.turn(a=turn_angle,bot=robot)     # pass angle as degrees
+        
+        # rotate to face destination
+        self.rot_to_dest(conv_x,conv_y,new_x,new_y,robot,move_robot)     
+        self.get_robot_pose(c)
 
-        # Start moving to destination
-        move_robot.move(d=dist/2,bot=robot)
+
         
